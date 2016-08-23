51883c76510d9d11aea68fe9d8009d1d0034e9dd
giancataraja <gccataraja@outlook.com>
2016-08-23 20:58:22 +0800

    Adding of about page

---
+++ about-content.php
@@ -0,0 +1,25 @@
+<h1 class="page-title">About US</h1>
+<h3 class="page-subtitle">Our Vision</h3>
+<q>A premiere institution committed to provide quality, affordable, efficient and person-centered healthcare.</q>
+
+<h3 class="page-subtitle">Mission</h3>
+<q>University of Cebu Medical Center aims to be a leader in providing comprehensive and quality healthcare as well as maintaining value for its patients and stakeholders.</q>
+
+<h3 class="page-subtitle">Goals</h3>
+<p>I. Provide exceptional quality patient-centered service by empowering and engaging the patient on their own healthcare journey</p>
+<p>II. Promote excellence in educational programs and the training of medical and paramedical professionals while encouraging/facilitating educational and professional advancement.</p>
+<p>III. Encourage ethical research activities essential for medical advancement.</p>
+<p>IV. Ensure the viability and sustainability of the institution.</p>
+<p>V. Provide a challenging and rewarding environment where associates are encouraged to grow personally and professionally and foster a relationship of trust among the associates based on an open communication and a spirit of collaboration, cooperation, and respect.</p>
+<p>VI. Foster linkages and networking with agencies and other similar facilities for complementation and sharing of resources.</p>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
