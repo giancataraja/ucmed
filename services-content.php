bf9d5d99f587ccdf4f8d75d810c57d8db75405e1
giancataraja <gccataraja@outlook.com>
2016-08-23 20:41:16 +0800

    Setting up projrct

---
+++ services-content.php
@@ -0,0 +1,177 @@
+<h1 class="page-title">Services</h1>
+<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingOne">
+      <h4 class="panel-title">
+        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          <h4 class="page-contenttitle"><span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>CARDIO DIAGNOSTIC UNIT</h4>
+        </a>
+      </h4>
+    </div>
+    <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+      <div class="panel-body">
+         <p><strong>CARDIODIAGNOSTIC UNIT LIST OF PROCEDURES:</strong></p>
+			<ul>
+				<li>2D echo with doppler adult and pedia</li>
+				<li>Treadmill stress test</li>
+				<li>Exercise stress echo</li>
+				<li>Dobutamine stress echo</li>
+				<li>24 hour holter monitoring</li>
+				<li>ECG</li>
+				<li>Ambulatory BP monitoring</li>
+			</ul>	
+		    <br>
+		
+			<p><strong>VASCULAR PROCEDURES</strong></p>
+			<ul>
+				<li>Carotid duplex scan</li>
+				<li>Arterial and venous duplex scan of upper and lower extremities</li>
+				<li>Renal artery duplex scan</li>
+				<li>Abdominal aorta study</li>
+				<li>Evaluation of AV shunt</li>
+
+			</ul>
+      </div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingTwo">
+      <h4 class="panel-title">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          <h4 class="page-contenttitle"><span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>DIALYSIS CENTER</h4>
+        </a>
+      </h4>
+    </div>
+    <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+      <div class="panel-body">
+		<ul>
+			<li>Regular dialysis treatment 4 hours</li> 
+			<li>SLED Dialysis 6 hrs</li>
+			<li>STAT hemodialysis</li>
+		</ul>
+      </div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingThree">
+      <h4 class="panel-title">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+          <h4 class="page-contenttitle"><span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>ENDOSCOPY UNIT</h4>
+        </a>
+      </h4>
+    </div>
+    <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+      <div class="panel-body">
+         <ul>
+			<li>Gastroscopy</li>
+			<li>Colonoscopy</li>
+			<li>Flexible sigmoidoscopy</li>
+			<li>Rubber band ligation for eosophageal varices</li>
+			<li>ERCP</li>
+			<li>Polypectomy both for upper & lower procedures</li>
+			<li>Peg insertion</li>
+			<li>Bronchoscopy</li>
+		</ul>
+      </div>
+    </div>
+  </div>
+  
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingFive">
+      <h4 class="panel-title">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+          <h4 class="page-contenttitle"><span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>LABORATORY DEPARTMENT</h4>
+        </a>
+      </h4>
+    </div>
+    <div id="collapseFive" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFive">
+      <div class="panel-body">
+		<p>The content of this area will be added soon.</p>
+      </div>
+    </div>
+  </div>
+  
+   <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingSeven">
+      <h4 class="panel-title">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
+          <h4 class="page-contenttitle"><span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>RADIOLOGY DEPARTMENT</h4>
+        </a>
+      </h4>
+    </div>
+    <div id="collapseSeven" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingSeven">
+      <div class="panel-body">
+        <ul>
+			<li>General radiography</li>
+			<li>Computed tomography (CT)</li>
+			<li>Magnetic resonance imaging (MRI)</li>
+			<li>Ultrasound</li>
+			<li>3d mammography</li>
+			<li>Cardiac catheterization laboratory</li>
+
+		</ul>
+      </div>
+    </div>
+  </div>
+  
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingEight">
+      <h4 class="panel-title">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseEight" aria-expanded="false" aria-controls="collapseEight">
+          <h4 class="page-contenttitle"><span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>WOMENS HEALTH CENTER</h4>
+        </a>
+      </h4>
+    </div>
+    <div id="collapseEight" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingEight">
+      <div class="panel-body">
+        <ul>
+			<li>2D ultrasound</li>
+			<li>3D/4D ultrasound</li>
+			<li>3D/4D ultrasound (twins)</li>
+			<li>Additional mammogram view </li>
+			<li>Amniocentesis</li>
+			<li>Amnioreduction</li>
+			<li>Biophysical profile</li>
+			<li>Biophysical profile (twins)</li>
+			<li>Colposcopy (procedure alone)</li>
+			<li>Congenital anomaly scan</li>
+			<li>Congenital anomaly scan (twins)</li>
+			<li>Consultation fee</li>
+			<li>Core needle biopsy</li>
+			<li>Doppler velocemitry</li>
+			<li>Doppler velocemitry (twins)</li>
+			<li>Drainage of abscess</li>
+			<li>Fine needle aspiration biopsy</li>
+			<li>Galactography</li>
+			<li>Mammo guided core/stereotactic needle biopsy</li>
+			<li>Mammo guided wire localization</li>
+			<li>Mammo w/ implant w/o tomosynthesis</li>
+			<li>Mammogram with tomosynthesis (3D) (bilateral)</li>
+			<li>Mammogram with tomosynthesis (3D) unilateral</li>
+			<li>Nuchal translucency scan</li>
+			<li>Nuchal translucency scan (twins)</li>
+			<li>PAP smear </li>
+			<li>Pelvic / transabdominal ultrasound</li>
+			<li>Pelvic / transabdominal ultrasound (twins)</li>
+			<li>Receptor assay (after a positive cancer biopsy)</li>
+			<li>Skin marking</li>
+			<li>Sono: skin marking</li>
+			<li>Sonohysterogram/saline infusion sonohysterography</li>
+			<li>Sonomammogram / breast ultrasound</li>
+			<li>Stereotactic core needle biopsy (all inclusive w/o PF)</li>
+			<li>Transrectal ultrasound</li>
+			<li>Transvaginal ultrasound</li>
+			<li>Transvaginal ultrasound (twins)</li>
+			<li>Ultrasound guided core (bilateral)</li>
+			<li>Ultrasound guided core (unilateral)</li>
+			<li>Ultrasound guided fine needle aspiration biopsy (bilateral)</li>
+			<li>Ultrasound guided fine needle aspiration biopsy (unilateral)</li>
+			<li>Ultrasound guided wire localization</li>
+			<li>Ultrasound-guided core needle biopsy(bilateral)</li>
+			<li>Ultrasound-guided core needle biopsy(unilateral)</li>
+
+		</ul>
+      </div>
+    </div>
+  </div>
+</div>
